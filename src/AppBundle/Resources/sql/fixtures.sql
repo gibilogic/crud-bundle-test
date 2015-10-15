a3a4dd475c74f9b99eb9f194402d877c5ff82cb2
Matteo Guindani <m.guindani@gibilogic.com>
2015-10-15 14:37:14 +0200

    Added SQL fixtures

---
+++ src/AppBundle/Resources/sql/fixtures.sql
@@ -0,0 +1,20 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.12deb2
+-- http://www.phpmyadmin.net
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `crud_bundle_test`
+--
+
+--
+-- Table `test_entity`
+--
+
+INSERT INTO `test_entity` (`id`, `name`, `created_at`) VALUES
+('00000001', 'Pippo', '2015-10-01 00:00:00'),
+('00000002', 'Pluto', '2015-10-02 00:00:00'),
+('00000003', 'Paperino', '2015-10-03 00:00:00'),
+('00000004', 'Paperoga', '2015-10-03 00:00:00');
